42817b6978c1548eac68a16a19cdac4db98a4e18
_An_Jin_ <aji0719@gmail.com>
2022-06-23 17:22:08 +0900

    1.4

---
+++ src/Greetings.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type GreetingProps = {
+  name: string;
+  mark?: string;
+}
+function Greetings({ name, mark } : GreetingProps) {
+  return (
+    <div>
+      Hello {name} {mark}
+    </div>
+  )
+}
+Greetings.defaultProps = {
+  mark: '!'
+}
+
+export default Greetings;
