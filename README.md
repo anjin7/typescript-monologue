16c676752bfcba59fec992ee6a9c0ade370553d4
_An_Jin_ <aji0719@gmail.com>
2022-07-08 19:01:54 +0900

    1.0.6 Home_board

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ https://anjin7.github.io/typescript-monologue/
 ### 구상ing...
 
 1. 게시판
-2. to-do
-3. random 챌린지(30일 챌린지 중 한가지 랜덤으로 선택) -> todo로 등록되도록
+2. to-do (+ random challenge )
 
 ### 계획ing...
 
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -3,13 +3,43 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   height: 1800px;
+  padding-top: 100px;
+  padding-left: 40px;
 `;
 
 function Home() {
   return (
     <Container>
-      <div></div>
-
+      <div>
+        <table>
+          <colgroup>
+            <col width="40px" />
+            <col width="400px" />
+            <col width="100px" />
+            <col width="40px" />                    
+          </colgroup>
+          <thead>
+            <td>#</td>
+            <td></td>
+            <td>Date</td>
+            <td>delete</td>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>abcdefg</td>
+              <td>2022-07-19</td>
+              <td>a</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>abcd</td>
+              <td>2022-07-19</td>
+              <td>a</td>
+            </tr> 
+          </tbody>                
+        </table>
+      </div>
     </Container>
   )
 }
