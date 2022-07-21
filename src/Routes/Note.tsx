c626eb20bdf3da2a746c7ff048deaefcce144c65
_An_Jin_ <aji0719@gmail.com>
2022-07-21 22:30:22 +0900

    1.0.11 note

--- src/Routes/Note.tsx
+++ src/Routes/Note.tsx
@@ -4,20 +4,36 @@ const Container = styled.div`
   padding: 80px 100px;
   height: 100vh;
 `;
-const NoteList = styled.ul`
+const NoteCard = styled.div`
   margin: 20px;
-`
-
+  width: 200px;
+  height: 300px;
+  border: 1px solid #87ceeb;
+  background-color: rgba(255,255,255,0.3);
+`;
+const NoteInput = styled.input`
+  width: 180px;
+  margin: 0 10px;
+`;
+const NoteBtn = styled.button`
+  background-color: #87ceeb;
+  color: gray;
+  border: 1px solid gray;
+  width: 180px;
+  margin: 0 10px;
+`;
 
 function Note() {
   return (
     <Container>
-      <NoteList>
-        <input></input>
-      </NoteList>
-      <NoteList>
-        <input></input>
-      </NoteList>
+      <NoteCard>
+        <form>
+          <NoteInput type="text"></NoteInput>
+          <NoteInput type="text"></NoteInput>
+          <br />
+          <NoteBtn>Add</NoteBtn>
+        </form>        
+      </NoteCard>
     </Container>
   )
 };
